c46df49be0c927a23f5a6a1cb3599e6d34e18f14
jajhog <majhog2@aol.com>
2015-12-10 08:52:38 +0900

    the first

---
+++ new.py
@@ -0,0 +1,6 @@
+def new(num_buckets=256):
+    """Initializes a Map with the given number of buckets."""
+    aMap = []
+    for i in range(0, num_buckets):
+        aMap.append([])
+    return aMap
