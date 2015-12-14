920dc5f2773d85213f8551ac56bcb80b24a27bbe
jajhog <majhog2@aol.com>
2015-12-15 08:32:37 +0900

    abc

--- hash.py
+++ hash.py
@@ -1,6 +1,11 @@
 import new
 
+aMap = ['abc','efg']
+key = 'osa','tky'
+
 def hash_key(aMap, key):
     """Given a key this will create a number and then convert it to
     an index for the aMap's buckets."""
     return hash(key) % len(aMap)
+	
+print aMap
