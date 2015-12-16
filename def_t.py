6c68bc4fcb9510076eccb4f9781fc2cb98d17441
jajhog <majhog2@aol.com>
2015-12-16 09:20:59 +0900

    dd

---
+++ def_t.py
@@ -0,0 +1,5 @@
+def abc(x):
+	x = raw_input("Please enter name:\n")
+	print "your name is: %s" % x
+
+
---
+++ def_t1.py
@@ -0,0 +1,6 @@
+from def_t import abc
+
+y = raw_input('hello! pleas enter your company name:\n')
+
+abc(y)
+
