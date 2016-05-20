a142b3906cccc249d5e0f7691f564daa5ee765f2
jajhog <majhog2@aol.com>
2016-05-20 09:27:20 +0900

    no message

---
+++ mystuff.py
@@ -0,0 +1,8 @@
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print "I AM CLASSY APPLES!"
+That looks complicated compared to modules, and the
